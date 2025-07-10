26b666ccd96df7a8429f12fbd9f8c12f7884943c
nineowngoals <88162117+nineowngoals@users.noreply.github.com>
2025-07-11 03:46:29 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ paymentUrl }) {
 
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      Redirecting to payment page...
+  
     </div>
   );
 }
@@ -18,8 +18,7 @@ export default function Home({ paymentUrl }) {
 export async function getServerSideProps(context) {
   const invoiceId = context.query.inv || Date.now().toString();
   const NOWPAYMENTS_API_KEY = "HF7WQ8T-DN3MYYY-Q145GEH-84W8NSV";
-  const amountAED = 4500;
-  const amountUSD = (amountAED / 3.67).toFixed(2);
+  const amountUSD = 450;
 
   try {
     const response = await axios.post(
@@ -29,9 +28,8 @@ export async function getServerSideProps(context) {
         price_currency: "usd",
         pay_currency: "usdttrc20",
         order_id: invoiceId,
-        order_description: "Consulting & Facilitation Fee - Kayuaya FZ LLC",
-        success_url: "https://invoices.vercel.app/success",
-        cancel_url: "https://invoices.vercel.app/cancel"
+        order_description: "Shipment Document Processing for " + invoiceId,
+        success_url: "https://twcargo.com/thank-you/"
       },
       {
         headers: {
